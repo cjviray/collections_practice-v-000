7aa25a4dc839d99a3626774e50d04702e8db4c9a
Christian V <viray.christianj@gmail.com>
2018-12-11 23:52:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array-desc(array)
+  array.sort.reverse
+  end 
+end
+
+def sort_array_char_count(string)
+  string.sort
+  a.length <=> b.length
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  end 
+end
+
+def reverse_array
+  
+end 
+
+def kesha_maker
+  every
+end 
+
+def find_a
+  start_with? (a)
+end 
+
+def sum_array(sum)
+    sum = + 
+  return sum 
+end 
+
+def adds_s(word)
+  word.collect
+  return word + "s"
+end 
